bf13d2c5aae0ce66518b5f7ed869f5388d1f6a40
astorelloo astorelloo <lil.astor06@gmail.com>
2024-11-04 13:50:33 +0000

    QUASI FINITO

---  first-rest-api.py
+++  first-rest-api.py
@@ -1,36 +1,32 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify # nella CLI: pip install flask 
 import mysql.connector
 import json
 import pymysql #nella CLI pip install pymysql
 
 #Connect to mysql
 mydb = mysql.connector.connect(
-  host="localhost",
-  user="root",
-  password="",
-  database="Animali"
+host="localhost",
+user="root",
+password="",
+database="Animali"
 )
 mycursor = mydb.cursor()
-
 app = Flask(__name__)
 
-def hello():
-    return "Hello, World!"
-
 def getAllData():
-    mtcursor = mysql.connection.cursor()
     mycursor.execute("SELECT * FROM Mammiferi")
     rows = mycursor.fetchall()
     result = [];
-    for x in myresult:
+    for x in rows:
         print(x);
         result.append(x);
     return result
 
 def getByRazza(razza): #nome del metodo(variabile interessata)
     query= " SELECT * FROM Mammiferi WHERE razza = %s"
-    mycursor.execute(query,(razza))
+    mycursor.execute(query,(razza,))
     rows = mycursor.fetchall()
+    return rows
 
 @app.route("/")
 def index():
@@ -40,7 +36,7 @@ def index():
 @app.route("/<razza>")
 def Koala(razza):
     data = getByRazza(razza)
-    return jsonify({razz: data})
+    return jsonify({razza: data})
 
 if __name__ == "__main__":
     app.run()
